dbb215ab31d107e4bce251755de2b71d89cb5740
Kuba Kopeć <42216586+kuba2kopec@users.noreply.github.com>
2021-05-28 18:02:13 +0200

    szkielet zadania

---
+++ main.py
@@ -0,0 +1,16 @@
+contacts = []
+
+while True:
+  m = int(input("Wybierz funkcje:\n1 - dodaj nowy kontakt\n2 - edytuj kontakt\n3 - usun kontakt\n4 - pokaz wszystkie kontakty\n5 - wyszukaj kontakt"))
+  if m == 1:
+    # dodanie kontaktu
+  elif m == 2:
+    # edycja kontaktu
+  elif m == 3:
+    # usuwanie kontaktu
+  elif m == 4:
+    # pokazanie listy
+  elif m == 5:
+    # wyszukanie kontaktu
+  else:
+    print("niepoprawna opcja")
