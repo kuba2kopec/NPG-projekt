55aa9638541beb29a379a13bf746ca1d4d03ed63
Kuba Kopeć <kuba2kopec@gmail.com>
2021-06-07 21:44:20 +0200

    małe poprawki

--- main.py
+++ main.py
@@ -13,36 +13,36 @@ def search () -> str:
   zmienna = ""
   if m == "1":
     zmienna = str(input("Podaj imie osoby szukanej:\n"))
-  if m == "2":
+  elif m == "2":
     zmienna = str(input("Podaj nazwisko osoby szukanej:\n"))
-  if m == "3":
+  elif m == "3":
     zmienna = str(input("Podaj mail osoby szukanej:\n"))
-  if m == "4":
+  elif m == "4":
     zmienna = str(input("Podaj telefon osoby szukanej:\n"))
-  if m == "5":
+  elif m == "5":
     zmienna = str(input("Podaj ades osoby szukanej:\n"))
   else:
-    zmienna = "Not Defined"
-    return "Nieprawidłowa akcja"
+    print("Nieprawidłowa akcja")
+    return
   found = 0
   for contact in contacts:
-    if zmienna == contact.name:
+    if m == "1" and zmienna == contact.name:
       found = 1
-      print('contact.name contact.lastname contact.mail contact.phone contact.address'.format(contact=contact))
-    if zmienna == contact.lastname:
+      print('{contact.name} {contact.lastname} {contact.mail} {contact.phone} {contact.address}'.format(contact=contact))
+    elif m == "2" and zmienna == contact.lastname:
       found = 1
-      print( 'contact.name contact.lastname contact.mail contact.phone contact.address'.format(contact=contact))
-    if zmienna == contact.mail:
+      print( '{contact.name} {contact.lastname} {contact.mail} {contact.phone} {contact.address}'.format(contact=contact))
+    elif m == "3" and zmienna == contact.mail:
       found = 1
-      print( 'contact.name contact.lastname contact.mail contact.phone contact.address'.format(contact=contact))
-    if zmienna == contact.phone:
+      print( '{contact.name} {contact.lastname} {contact.mail} {contact.phone} {contact.address}'.format(contact=contact))
+    elif m == "4" and zmienna == contact.phone:
       found = 1
-      print('contact.name contact.lastname contact.mail contact.phone contact.address'.format(contact=contact))
-    if zmienna == contact.address:
+      print('{contact.name} {contact.lastname} {contact.mail} {contact.phone} {contact.address}'.format(contact=contact))
+    elif m == "5" and zmienna == contact.address:
       found = 1
-      print('contact.name contact.lastname contact.mail contact.phone contact.address'.format(contact=contact))
+      print('{contact.name} {contact.lastname} {contact.mail} {contact.phone} {contact.address}'.format(contact=contact))
   if found == 0:
-    return "Nie ma takiego kontaktu"
+    print("Nie ma takiego kontaktu")
 
 while True:
   m = str(input("Wybierz funkcje:\n1 - dodaj nowy kontakt\n2 - edytuj kontakt\n3 - usun kontakt\n4 - pokaz wszystkie kontakty\n5 - wyszukaj kontakt\n"))
